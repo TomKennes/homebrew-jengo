c8879dfe6b023b5ce91d2b11f4fff39fed151384
Tom Kennes <tomkennes@gskyworkz.nl>
2020-11-12 21:26:48 +0100

    add jengo_rb

---
+++ jengo.rb
@@ -0,0 +1,31 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class Jengo < Formula
+  desc "Golang based CLI for Jenkins API"
+  homepage "https://github.com/TomKennes/jengo"
+  url "https://github.com/TomKennes/jengo/archive/v1.1.1.tar.gz"
+  sha256 "31fb8de0966a12463bd00df0b36538f3da7bd6f995834f3edfc8f1ad3fb79560"
+  license "MIT"
+
+  depends_on "go" => :build
+
+  def install
+    system "go", "build", "-ldflags",
+             "-s -w -X main.version=#{version} -X main.commit=#{stable.specs[:revision]} -X main.builtBy=homebrew",
+             *std_go_args
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test jengo`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "false"
+  end
+end
